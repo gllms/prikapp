2d7213236f090709ba5537b0bcc76ce86d013816
Vincent <43859503+Th3rmalite@users.noreply.github.com>
2021-11-16 14:22:41 +0100

    json package

--- svelte-app/App.svelte
+++ svelte-app/App.svelte
@@ -1,1 +1,12 @@
 <p>Hello, World!</p>
+
+<script>
+    import * as myjson from './locations.json'
+    console.log(myjson);
+
+
+    export let getAllLocations;
+
+    getAllLocations();
+</script>
+
--- svelte-app/main.js
+++ svelte-app/main.js
@@ -1,13 +1,28 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body
+	target: document.body,
+    props: {
+        getAllLocations: async function() {
+            var locations = [];
+            let page = await  fetch("https://api.codetabs.com/v1/proxy/?quest=https://www.star-shl.nl/locaties-functiebeeldvormend-onderzoek/")
+            .then(function (response) {
+                // The API call was successful!
+                return response.text();
+            }).then(function (html) {
+                // This is the HTML from our response as a text string
+                //console.log(html);
+            }).catch(function (err) {
+                // There was an error
+                console.warn('Something went wrong.', err);
+            });
+
+        
+            return locations
+        },
+    }
 });
 
-function getAllLocations() {
-    var locations = [];
-    
-    return locations
-}
+
 
 export default app;
