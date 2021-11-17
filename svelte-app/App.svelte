7d1f8558e6ca5d46196d537421d240831dcc9e45
gllms <willemvanderdoe@hotmail.com>
2021-11-17 16:43:04 +0100

    Remove unnecessary dependency

--- svelte-app/App.svelte
+++ svelte-app/App.svelte
@@ -1,12 +1,1 @@
-<script>
-    let test = "hoi";
-    console.log(test);
-
-    fetch('./locations.json')
-        .then(response => response.json())
-        .then(data => test = data)
-        .catch(err => alert(err))
-</script>
-
 <p>Hello, World!</p>
-<p>{test[0].Place}</p>
