675549417dffd07cfe33cbb262e9c91e06b3684e
Thermalight <alumite10@gmail.com>
2021-11-28 20:53:10 +0100

    media queries

--- svelte-app/Locations.svelte
+++ svelte-app/Locations.svelte
@@ -1,4 +1,6 @@
 <script>
+    import MediaQuery from "./MediaQuery.svelte";
+
     let name = "";
     let locations = "Loading...";
     const Http = new XMLHttpRequest();
@@ -19,106 +21,159 @@
     <input type='text' bind:value={name}/>
 </div>
 
-
-<!-- <table class="data-table">
-    <thead>
-        <tr>
-            <th>Plaats</th>
-            <th>Naam Locatie</th>
-            <th>Adres</th>
-            <th>Postcode</th>
-            <th>Openingstijden</th>
-            <th>Bijzonderheden</th>
-        </tr>
-    </thead>
-    <tbody>
+<MediaQuery query="(min-width: 480px)" let:matches>
+    {#if matches}
+        <div class="location-table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Plaats</th>
+                        <th>Naam Locatie</th>
+                        <th>Adres</th>
+                        <th>Postcode</th>
+                        <th>Openingstijden</th>
+                        <th>Bijzonderheden</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each locations as location}
+                        <tr>
+                            <td>{@html location.Place}</td>
+                            <td>{@html location.LocationName}</td>
+                            <td>{location.Address}</td>
+                            <td>{location.Postalcode}</td>
+                            <td>{@html location.OpeningHours}</td>
+                            <td>{location.Particularities}</td>
+                        </tr>
+                    {/each}
+                </tbody>
+                
+            </table>
+        </div>
+        
+    {:else}
         {#each locations as location}
-            <tr>
-                <td>{@html location.Place}</td>
-                <td>{location.LocationName}</td>
-                <td>{location.Address}</td>
-                <td>{location.Postalcode}</td>
-                <td>{@html location.OpeningHours}</td>
-                <td>{location.Particularities}</td>
-            </tr>
+            <div class="location-card">
+                <table class="lightblue">
+                    <tr>
+                        <th>Plaats</th>
+                        <td>{@html location.Place}</td>
+                    </tr>
+                    <tr>
+                        <th>Naam</th>
+                        <td>{@html location.LocationName}</td>
+                    </tr>
+                    <tr>
+                        <th>Adres</th>
+                        <td>{location.Address}</td>
+                    </tr>
+                    <tr>
+                        <th>Postcode</th>
+                        <td>{location.Postalcode}</td>
+                    </tr>
+                    <tr>
+                        <th>Openingstijden</th>
+                        <td>{@html location.OpeningHours}</td>
+                    </tr>
+                    <tr>
+                        <th>Bijzonderheden</th>
+                        <td>{location.Particularities}</td>
+                    </tr>
+                </table>
+            </div>
         {/each}
-    </tbody>
-    
-</table> -->
+    {/if}
+</MediaQuery>
 
-{#each locations as location}
-    <div class="location-card">
-        <table class="lightblue">
-            <tr>
-                <th>Plaats</th>
-                <td>{@html location.Place}</td>
-            </tr>
-            <tr>
-                <th>Naam</th>
-                <td>{location.LocationName}</td>
-            </tr>
-            <tr>
-                <th>Adres</th>
-                <td>{location.Address}</td>
-            </tr>
-            <tr>
-                <th>Postcode</th>
-                <td>{location.Postalcode}</td>
-            </tr>
-            <tr>
-                <th>Openingstijden</th>
-                <td>{@html location.OpeningHours}</td>
-            </tr>
-            <tr>
-                <th>Bijzonderheden</th>
-                <td>{location.Particularities}</td>
-            </tr>
-        </table>
-    </div>
-{/each}
+
 
 <style>
-    .search {
-        text-align: center;
-        margin-bottom: 15px;
-        background-color: #f6f6f6;
-        width: max-content;
-        padding: 15px;
-        margin-left: auto;
-        margin-right: auto;
+    @media only screen and (max-width: 480px) {
+        .search {
+            text-align: center;
+            margin-bottom: 15px;
+            background-color: #f6f6f6;
+            width: max-content;
+            padding: 15px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .location-card {
+            padding: 5px;
+        }
+
+        table {
+            width: 100%;
+            border-radius: 8px;
+            margin: 0 auto 10px auto;
+            border-collapse:collapse;
+            color: black;
+        }
+
+        tr:first-child {
+            background-color: #e7334c;
+        }
+
+        table, th, td {
+            padding: 5px;
+            border: 1px solid white;
+        }
+
+        th {
+            width: 100px;
+            text-align: right;
+            font-weight: bolder;
+        }
+
+        td {
+            text-align: left;
+            font-weight: 50;
+        }
     }
 
-    .location-card {
-        padding: 5px;
-    }
+    @media only screen and (min-width: 480px) {
+        .location-table {
+            margin: auto;
+        }
 
-    table {
-        width: 100%;
-        border-radius: 8px;
-        margin: 0 auto 10px auto;
-        border-collapse:collapse;
-        color: black;
-    }
+        .search {
+            text-align: center;
+            margin-bottom: 15px;
+            background-color: #f6f6f6;
+            width: max-content;
+            padding: 15px;
+            margin-left: auto;
+            margin-right: auto;
+        }
 
-    tr:first-child {
-        background-color: #e7334c;
-    }
+        table {
+            width: 100%;
+            border-radius: 8px;
+            margin: 0 auto 10px auto;
+            border-collapse:collapse;
+            color: black;
+            background-color: #79b9d5;
+        }
 
-    table, th, td {
-        padding: 5px;
-        border: 1px solid white;
-    }
+        table, th, td {
+            padding: 5px;
+            border: 1px solid white;
+        }
 
-    th {
-        width: 100px;
-        text-align: right;
-        font-weight: bolder;
-    }
+        th {
+            width: 100px;
+            text-align: left;
+            font-weight: bolder;
+            background-color: #e7334c;
+        }
 
-    td {
-        text-align: left;
-        font-weight: 550;
+        td {
+            text-align: left;
+            font-weight: 50;
+        }
     }
+    
     
     /* colors */
 
