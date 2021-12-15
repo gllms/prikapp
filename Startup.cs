4eb1d720e110c8c77b38470581185a31488532a3
Thermalight <alumite10@gmail.com>
2021-12-15 09:27:31 +0100

    added compression namespace

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO.Compression;
 
 namespace prikapp
 {
